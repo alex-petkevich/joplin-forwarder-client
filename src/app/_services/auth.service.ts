ab51b58ac98527cc6c60da0fe57c95d886194c16
alex-petkevich <a@homesite.by>
2022-05-03 23:17:19 +0200

    Auth system - mails

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'http://localhost:8080/api/account/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
