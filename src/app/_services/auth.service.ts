cec19904f8476fc14c1176f4ada626e817a2454a
alex-petkevich <a@homesite.by>
2022-05-24 23:18:37 +0200

    Auth system - upload files

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../environments/environment";
 
-const AUTH_API = 'http://localhost:8080/api/account/';
+const AUTH_API = 'api/account/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -15,14 +16,14 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
+    return this.http.post(environment.backendUrl + AUTH_API + 'signin', {
       username,
       password
     }, httpOptions);
   }
 
   register(username: string, email: string, password: string, lastname: string, firstname: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signup', {
+    return this.http.post(environment.backendUrl + AUTH_API + 'signup', {
       username,
       email,
       password,
@@ -32,24 +33,24 @@ export class AuthService {
   }
 
   activate(key: string): Observable<any> {
-    return this.http.post(AUTH_API + 'activate', {
+    return this.http.post(environment.backendUrl + AUTH_API + 'activate', {
       key
     }, httpOptions);
   }
 
   forgotPassword(email: string): Observable<any> {
-    return this.http.get(AUTH_API + 'forgot-password/' + email, { responseType: 'json' });
+    return this.http.get(environment.backendUrl + AUTH_API + 'forgot-password/' + email, { responseType: 'json' });
   }
 
   passwordReset(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'password-reset/', {
+    return this.http.post(environment.backendUrl + AUTH_API + 'password-reset/', {
       username,
       password
     }, httpOptions);
   }
 
   checkKey(key: string): Observable<any> {
-    return this.http.post(AUTH_API + 'check-key', {
+    return this.http.post(environment.backendUrl + AUTH_API + 'check-key', {
       key
     }, httpOptions);
   }
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
-const API_URL = 'http://localhost:8080/api/user/';
+import {environment} from "../environments/environment";
+const API_URL = environment.backendUrl + 'api/user/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
