4caf8d1e13431f6c7fb3e53d0900cedc3c25a251
Aliaksandr Piatkevich <aliaksandr.piatkevich@rbi-group-it.com>
2022-09-13 16:12:21 +0200

    add logos and FE changes

--- src/app/account/forgot-password/forgot-password.component.ts
+++ src/app/account/forgot-password/forgot-password.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../_services/auth.service";
 import {TokenStorageService} from "../../_services/token-storage.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,7 @@ export class ForgotPasswordComponent implements OnInit {
   errorMessage = '';
   isLoggedIn = false;
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) {
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -33,6 +34,13 @@ export class ForgotPasswordComponent implements OnInit {
         this.tokenStorage.saveUser(data);
         this.isSentSucessfully = true;
         this.isSentFailed = false;
+        if (data.lang) {
+          this.tokenStorage.saveLang(data.lang);
+        }
+
+        this.router.navigate(['/home']).then(() => {
+          window.location.reload();
+        });
       } ,
       error: err => {
         this.errorMessage = err.error.message;
